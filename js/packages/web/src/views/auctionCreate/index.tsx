e0fdcc00228917f42126f846740dc41d07eb4b59
Yeabsra Hailu <yeabsdebe@gmail.com>
2022-05-20 12:27:06 +0300

    Minor fix

--- js/packages/web/src/views/auctionCreate/index.tsx
+++ js/packages/web/src/views/auctionCreate/index.tsx
@@ -708,7 +708,7 @@ const CategoryStep = (props: {
       <Row className="call-to-action">
         <h2>List your Music NFT</h2>
         <p>
-          First time listing on Metaplex? <a>Read our sellers&apos; guide.</a>
+          First time listing on Helios? <a>Read our sellers&apos; guide.</a>
         </p>
       </Row>
       <Row justify={width < 768 ? 'center' : 'start'}>
@@ -2052,7 +2052,7 @@ const WaitingStep = (props: {
     >
       <Progress type="circle" percent={progress} />
       <div className="waiting-title">
-        Your creation is being listed with Metaplex...
+        Your creation is being listed with Helios...
       </div>
       <div className="waiting-subtitle">This can take up to 30 seconds.</div>
     </div>
@@ -2070,13 +2070,13 @@ const Congrats = (props: {
 
   const newTweetURL = () => {
     const params = {
-      text: "I've created a new NFT auction on Metaplex, check it out!",
+      text: "I've created a new NFT auction on Helios, check it out!",
       url: `${
         window.location.origin
       }/#/auction/${props.auction?.auction.toString()}`,
-      hashtags: 'NFT,Crypto,Metaplex',
-      // via: "Metaplex",
-      related: 'Metaplex,Solana',
+      hashtags: 'NFT,Crypto,Helios',
+      // via: "Helios",
+      related: 'Helios,Solana',
     };
     const queryParams = new URLSearchParams(params).toString();
     return `https://twitter.com/intent/tweet?${queryParams}`;
